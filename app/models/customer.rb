4dca6fb242e15dc2032cfc6cd2abb4fec3e5dcbc
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2015-10-02 12:40:11 +0800

    Fix address method inside Customer model and add phone column inside view
    Also add some console.log to every Customer controller methods to check
    if it’s working after create/update/destroy

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,8 +3,12 @@ class Customer < ActiveRecord::Base
 	validates :first_name, presence: true, length: { minimum: 2 }
 	 validates :last_name, presence:true, length: {minimum: 2}
 	 
+	 # to be called in _customer.html.erb partial
+	 # example: Jalan Binjai, 12345, Kuala Lumpur
 	 def address
-		"#{street} #{zip} #{city}"
+	 	if street.present? && zip.present? && city.present?
+	 		"#{street}, #{zip}, #{city}"
+	 	end
 	 end
 	
 end
